5a0cac273a39e05eafd20781006384469444a9b6
Kimberly <kamueller2@gmail.com>
2019-05-29 12:41:51 -0400

    added event for user input

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,3 +9,11 @@ let yourGuess = [];
 let choices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 let randomGuess = choices[Math.floor(Math.random() * choices.length)];
+
+// Let's start by grabbing a reference to the <span> below.
+let userText = document.getElementById("user-text");
+
+// Next, we give JavaScript a function to execute when onkeyup event fires.
+document.onkeyup = function(event) {
+    userText.textContent = event.key;
+};
