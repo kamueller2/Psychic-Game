1c3f8dceb334120f7a28fe0451e67f91bfb65040
Kimberly <kamueller2@gmail.com>
2019-05-28 22:42:35 -0400

    added additional variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,3 +5,5 @@ let wins = 0;
 let losses = 0;
 let guessesLeft = 9;
 let yourGuess = [];
+
+let choices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
