51b53a4607a649a5e4122a896f794af0659015b8
Kimberly <kamueller2@gmail.com>
2019-05-29 14:52:11 -0400

    revised random numbers

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,14 +4,28 @@ let guessLetter = 'Guess what letter I/m thinking of';
 let wins = 0;
 let losses = 0;
 let guessesLeft = 9;
-let yourGuess = [];
+let yourGuesses = [];
+let compGuess = [];
 
 let choices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-let randomGuess = choices[Math.floor(Math.random() * choices.length)];
+//Computer letter
+window.onload = function() {
+    let randomGuess = choices[Math.floor(Math.random() * choices.length)];
+    compGuess.push(randomGuess);
+    console.log(compGuess);
+}
 
+//Game
+document.onkeyup = function(event) {
+    let playerGuess = event.key;
+    yourGuesses.push(playerGuess);
+    console.log(compGuess[0]);
+}
+
+if (playerGuess[0] !== compGuess[0])
 // Let's start by grabbing a reference to the <span> below.
-let userText = document.getElementById("user-text");
+    let userText = document.getElementById("user-text");
 
 // Next, we give JavaScript a function to execute when onkeyup event fires.
 document.onkeyup = function(event) {
