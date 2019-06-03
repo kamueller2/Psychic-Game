4c34b473527a2f50a04f9ebb338d7d0ce86cba57
Kimberly <kamueller2@gmail.com>
2019-06-03 10:57:13 -0400

    revised functions and else statement

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,33 +1,69 @@
-//assigning variables
-
-let guessLetter = 'Guess what letter I/m thinking of';
-let wins = 0;
-let losses = 0;
-let guessesLeft = 9;
-let yourGuesses = [];
-let compGuess = [];
-
+// declared variable to set and array of letters
 let choices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-//Computer letter
-window.onload = function() {
-    let randomGuess = choices[Math.floor(Math.random() * choices.length)];
-    compGuess.push(randomGuess);
-    console.log(compGuess);
+// declared variables 
+let wins = 0; // user wins
+let losses = 0; // user loses
+let left = 9; // guesses left
+let guesses = 9;
+let lettersGuessed = [];
+let psychic = "";
+
+//store all html 'nodes' that we need to dynamically update and we reference these elements via their ids
+
+let winsNum = document.getElementById("wins").innerHTML = wins;
+let lossNum = document.getElementById("losses").innerHTML = losses;
+let guessesLeft = document.getElementById("guesses-left").innerHTML = left;
+// let guessedLetter = document.getElementById("guesses").innerHTML = guesses;
+let allGuesses = document.getElementById("user-text").innerHTML = lettersGuessed;
+console.log(allGuesses);
+
+
+// helper functions
+let magic = function() {
+    psychic = choices[Math.floor(Math.random() * choices.length)];
+
+    // test
+    // psychic = document.getElementById("comp").innerHTML = psychic;
+    //console.log(psychic);
+};
+
+
+let soFar = function() {
+    document.getElementById("user-text").innerHTML = lettersGuessed.join(",");
+};
+
+let remainingGuess = function() {
+    document.getElementById("guesses-left").innerHTML = left;
+};
+
+let newGame = function() {
+    lettersGuessed = [];
+    left = 9;
+    magic();
+    remainingGuess();
+    soFar();
 }
 
-//Game
 document.onkeyup = function(event) {
     let playerGuess = event.key;
-    yourGuesses.push(playerGuess);
-    console.log(compGuess[0]);
+    left--;
+    lettersGuessed.push(playerGuess);
+    soFar();
+    remainingGuess();
+
+    if (left >= 0) {
+        if (playerGuess === psychic) {
+            wins++;
+            document.getElementById("wins").innerHTML = wins;
+            magic();
+        }
+    } else if (left < 0) {
+        losses++;
+        document.getElementById("losses").innerHTML = losses;
+        //alert("Game over, rematch?");
+        newGame();
+
+    }
+
 }
-
-if (playerGuess[0] !== compGuess[0])
-// Let's start by grabbing a reference to the <span> below.
-    let userText = document.getElementById("user-text");
-
-// Next, we give JavaScript a function to execute when onkeyup event fires.
-document.onkeyup = function(event) {
-    userText.textContent = event.key;
-};
